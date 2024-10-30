04a7c956cb87a661660151a8e508838cb3489838
Lázaro José <40708051+LazaroJPR@users.noreply.github.com>
2024-10-29 21:13:00 -0300

    Create Tarefa.kt

---
+++ src/main/kotlin/com/lazarojpr/lista_de_tarefas/model/Tarefa.kt
@@ -0,0 +1,24 @@
+package com.lazarojpr.lista_de_tarefas.model
+
+import jakarta.persistence.*
+import java.math.BigDecimal
+import java.time.LocalDate
+
+@Entity
+@Table(name = "tarefas")
+data class Tarefa(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    @Column(unique = true, nullable = false)
+    val nome: String,
+
+    @Column(nullable = false)
+    val custo: BigDecimal,
+
+    @Column(name = "data_limite", nullable = false)
+    val dataLimite: LocalDate,
+
+    @Column(name = "ordem_apresentacao", nullable = false, unique = true)
+    val ordemApresentacao: Int
+)
