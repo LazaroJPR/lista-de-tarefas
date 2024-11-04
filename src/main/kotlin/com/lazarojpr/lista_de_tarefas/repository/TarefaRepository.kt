caefaf6aae97602a4e9374d36ae0054141b02c2e
Lázaro José <40708051+LazaroJPR@users.noreply.github.com>
2024-11-04 12:42:33 -0300

    feat: add findMaxOrdemApresentacao query to retrieve maximum order presentation

--- src/main/kotlin/com/lazarojpr/lista_de_tarefas/repository/TarefaRepository.kt
+++ src/main/kotlin/com/lazarojpr/lista_de_tarefas/repository/TarefaRepository.kt
@@ -2,7 +2,13 @@ package com.lazarojpr.lista_de_tarefas.repository
 
 import com.lazarojpr.lista_de_tarefas.model.Tarefa
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.stereotype.Repository
 
+@Repository
 interface TarefaRepository : JpaRepository<Tarefa, Long>{
     fun findByNome(nome: String): Tarefa?
+
+    @Query("SELECT MAX(t.ordemApresentacao) FROM Tarefa t")
+    fun findMaxOrdemApresentacao(): Int?
 }
