d578c8a638e355d03cdcd16a430866dc73c142a4
Lázaro José <40708051+LazaroJPR@users.noreply.github.com>
2024-11-04 12:41:44 -0300

    feat: implement global exception handling for IllegalArgumentException

---
+++ src/main/kotlin/com/lazarojpr/lista_de_tarefas/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,15 @@
+package com.lazarojpr.lista_de_tarefas.exception
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+
+@ControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException::class)
+    fun handleIllegalArgumentException(ex: IllegalArgumentException): ResponseEntity<String> {
+        return ResponseEntity(ex.message, HttpStatus.BAD_REQUEST)
+    }
+}
