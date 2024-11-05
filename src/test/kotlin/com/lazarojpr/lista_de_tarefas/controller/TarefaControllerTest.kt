be71c521d72dbe70358d20bfb48cf613c0e1acff
Lázaro José <40708051+LazaroJPR@users.noreply.github.com>
2024-11-04 23:52:30 -0300

    test: refactor TarefaControllerTest to use @InjectMocks and add assertions for responses

--- src/test/kotlin/com/lazarojpr/lista_de_tarefas/controller/TarefaControllerTest.kt
+++ src/test/kotlin/com/lazarojpr/lista_de_tarefas/controller/TarefaControllerTest.kt
@@ -2,58 +2,63 @@ package com.lazarojpr.lista_de_tarefas.controller
 
 import com.lazarojpr.lista_de_tarefas.model.Tarefa
 import com.lazarojpr.lista_de_tarefas.service.TarefaService
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito
-import org.mockito.MockitoAnnotations
-import org.springframework.boot.test.context.SpringBootTest
+import org.mockito.junit.jupiter.MockitoExtension
+import org.springframework.http.HttpStatus
 import java.math.BigDecimal
 import java.time.LocalDate
+import kotlin.test.assertEquals
 
-@SpringBootTest
+@ExtendWith(MockitoExtension::class)
 class TarefaControllerTest {
-
-    private lateinit var tarefaController: TarefaController
-
     @Mock
     private lateinit var tarefaService: TarefaService
 
-    @BeforeEach
-    fun setup() {
-        MockitoAnnotations.openMocks(this)
-        tarefaController = TarefaController(tarefaService)
-    }
+    @InjectMocks
+    private lateinit var tarefaController: TarefaController
 
     @Test
     fun `deve criar uma tarefa`() {
-        val tarefa = Tarefa(
-            id = 1L,
-            nome = "Tarefa Exemplo",
-            custo = BigDecimal(100),
-            dataLimite = LocalDate.of(2024, 12, 31),
-            ordemApresentacao = 1
-        )
+        val tarefa =
+            Tarefa(
+                id = 1L,
+                nome = "Tarefa Exemplo",
+                custo = BigDecimal(100),
+                dataLimite = LocalDate.of(2024, 12, 31),
+                ordemApresentacao = 1,
+            )
 
         Mockito.`when`(tarefaService.salvarTarefa(tarefa)).thenReturn(tarefa)
-        tarefaController.criarTarefa(tarefa)
+
+        val response = tarefaController.criarTarefa(tarefa)
+
+        assertEquals(HttpStatus.OK, response.statusCode)
+        assertEquals(tarefa, response.body)
         Mockito.verify(tarefaService, Mockito.times(1)).salvarTarefa(tarefa)
     }
 
     @Test
     fun `deve atualizar uma tarefa`() {
-        val tarefa = Tarefa(
-            id = 1L,
-            nome = "Tarefa Atualizada",
-            custo = BigDecimal(200),
-            dataLimite = LocalDate.of(2024, 11, 30),
-            ordemApresentacao = 2
-        )
+        val tarefa =
+            Tarefa(
+                id = 1L,
+                nome = "Tarefa Atualizada",
+                custo = BigDecimal(200),
+                dataLimite = LocalDate.of(2024, 11, 30),
+                ordemApresentacao = 2,
+            )
 
-        val id = tarefa.id
-
+        val id = tarefa.id!! // Converte id para Long não nulo
         Mockito.`when`(tarefaService.atualizarTarefa(id, tarefa)).thenReturn(tarefa)
-        tarefaController.atualizarTarefa(id, tarefa)
+
+        val response = tarefaController.atualizarTarefa(id, tarefa)
+
+        assertEquals(HttpStatus.OK, response.statusCode)
+        assertEquals(tarefa, response.body)
         Mockito.verify(tarefaService, Mockito.times(1)).atualizarTarefa(id, tarefa)
     }
 
@@ -61,24 +66,42 @@ class TarefaControllerTest {
     fun `deve excluir uma tarefa`() {
         val id = 1L
 
-        tarefaController.excluirTarefa(id)
+        val response = tarefaController.excluirTarefa(id)
+
+        assertEquals(HttpStatus.NO_CONTENT, response.statusCode)
         Mockito.verify(tarefaService, Mockito.times(1)).excluirTarefa(id)
     }
 
     @Test
     fun `deve listar todas as tarefas`() {
-        val tarefas = listOf(
-            Tarefa(
-                id = 1L,
-                nome = "Tarefa Exemplo",
-                custo = BigDecimal(100),
-                dataLimite = LocalDate.of(2024, 12, 31),
-                ordemApresentacao = 1
+        val tarefas =
+            listOf(
+                Tarefa(
+                    id = 1L,
+                    nome = "Tarefa Exemplo",
+                    custo = BigDecimal(100),
+                    dataLimite = LocalDate.of(2024, 12, 31),
+                    ordemApresentacao = 1,
+                ),
             )
-        )
 
         Mockito.`when`(tarefaService.listarTarefas()).thenReturn(tarefas)
-        tarefaController.listarTarefas()
+
+        val response = tarefaController.listarTarefas()
+
+        assertEquals(HttpStatus.OK, response.statusCode)
+        assertEquals(tarefas, response.body)
         Mockito.verify(tarefaService, Mockito.times(1)).listarTarefas()
+    }
+
+    @Test
+    fun `deve trocar duas tarefas`() {
+        val id1 = 1L
+        val id2 = 2L
+
+        val response = tarefaController.trocarTarefas(id1, id2)
+
+        assertEquals(HttpStatus.NO_CONTENT, response.statusCode)
+        Mockito.verify(tarefaService, Mockito.times(1)).trocarTarefas(id1, id2)
     }
 }
