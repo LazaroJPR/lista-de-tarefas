8e2f62fe7228cc76f6ae4baa689f50f1fefb6116
Lázaro José <40708051+LazaroJPR@users.noreply.github.com>
2024-11-04 22:04:18 -0300

    refactor: enhance TarefaController formatting and improve readability

--- src/main/kotlin/com/lazarojpr/lista_de_tarefas/controller/TarefaController.kt
+++ src/main/kotlin/com/lazarojpr/lista_de_tarefas/controller/TarefaController.kt
@@ -5,23 +5,29 @@ import com.lazarojpr.lista_de_tarefas.service.TarefaService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
-
 @RestController
 @RequestMapping("/tarefas")
-class TarefaController (private val tarefaService: TarefaService){
-
+class TarefaController(
+    private val tarefaService: TarefaService,
+) {
     @GetMapping
     fun listarTarefas(): ResponseEntity<List<Tarefa>> = ResponseEntity.ok(tarefaService.listarTarefas())
 
     @PostMapping
-    fun criarTarefa(@RequestBody tarefa: Tarefa): ResponseEntity<Tarefa> = ResponseEntity.ok(tarefaService.salvarTarefa(tarefa))
+    fun criarTarefa(
+        @RequestBody tarefa: Tarefa,
+    ): ResponseEntity<Tarefa> = ResponseEntity.ok(tarefaService.salvarTarefa(tarefa))
 
     @PutMapping("/{id}")
-    fun atualizarTarefa(@PathVariable id: Long, @RequestBody tarefa: Tarefa): ResponseEntity<Tarefa> =
-        ResponseEntity.ok(tarefaService.atualizarTarefa(id, tarefa))
+    fun atualizarTarefa(
+        @PathVariable id: Long,
+        @RequestBody tarefa: Tarefa,
+    ): ResponseEntity<Tarefa> = ResponseEntity.ok(tarefaService.atualizarTarefa(id, tarefa))
 
     @DeleteMapping("/{id}")
-    fun excluirTarefa(@PathVariable id: Long): ResponseEntity<Void> {
+    fun excluirTarefa(
+        @PathVariable id: Long,
+    ): ResponseEntity<Void> {
         tarefaService.excluirTarefa(id)
         return ResponseEntity.noContent().build()
     }
